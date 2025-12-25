b6df6056a3a7f6e9162653a3582c68d91d61598a
Mateusz Gostański <mateusz.gostanski@gmail.com>
2025-12-25 11:27:38 +0100

    fix: ui color

--- app/app.config.ts
+++ app/app.config.ts
@@ -3,9 +3,10 @@ import { defineAppConfig } from "nuxt/app"
 export default defineAppConfig({
   ui: {
     colors: {
-      primary: "sky",
+      primary: "blue",
       secondary: "blue",
       neutral: "slate",
+      success: "blue",
     },
   },
 })
